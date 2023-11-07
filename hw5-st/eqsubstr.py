ae720a432eb56450e6fc892fc0ab3548708309a1
Sherry Fu <fuxueyi2580@gmail.com>
2023-11-06 23:41:30 -0500

    unfinished

--- hw5-st/eqsubstr.py
+++ hw5-st/eqsubstr.py
@@ -23,10 +23,43 @@
 #    (6, 4, 2), (11, 4, 2)}
 #  Note that the length 4 range of 'b's does not appear as there are no
 #  Length 4 runs of 'a's.
+import time
+import random
+from collections import deque
 
 def matching_length_sub_strs(s, c1, c2):
     # WRITE ME
-    return set()
+    result=set()
+    a_length={}
+    b_length={}
+    a_len=0
+    b_len=0
+    index=-1
+    for i, sub in enumerate(s):
+        if sub==c1:
+            if a_len==0:
+                index=i
+            a_len+=1
+            if (i+1)==len(s) or s[i]!=s[i+1]:
+                if a_len not in a_length:
+                    a_length[a_len]=[]
+                a_length[a_len].append(index)
+                a_len=0
+        if sub==c2:
+            if b_len==0:
+                index=i
+            b_len+=1
+            if (i+1)==len(s) or s[i]!=s[i+1]:
+                if b_len not in b_length:
+                    b_length[b_len]=[]
+                b_length[b_len].append(index)
+                b_len=0
+    for key in a_length:
+        if key in b_length:
+            for i in a_length[key]:
+                for j in b_length[key]:
+                    result.add((i,j,key))                         
+    return result
 
 
 # Makes a random string of length n
@@ -43,5 +76,29 @@ def rndstr(n):
         return 'b'
     ans=[rndchr() for i in range(n)]
     return "".join(ans)
+
+def best(n):
+    return 'a'*(n//2)+'b'*(n//2)
+
+def worst(n):
+    return 'ab'*(n//2)
+
+if __name__ == "__main__":
+    strgens=[best,worst,rndstr]
+    #strgens=[best]
+    sizes = [2**i for i in range(9, 15)]
+    #sizes = [2**i for i in range(9, 13)]
+    #sizes = [2**i for i in range(5,6)]
+    for size in sizes:
+        result=[]
+        result.append(size)
+        for sg in strgens:
+            s=sg(size)
+            start=time.time()
+            matching_length_sub_strs(s, 'a', 'b')
+            end=time.time()
+            runtime=end-start
+            result.append(runtime)
+        print(result[0],result[1],result[2],result[3],sep=",")
 
     
